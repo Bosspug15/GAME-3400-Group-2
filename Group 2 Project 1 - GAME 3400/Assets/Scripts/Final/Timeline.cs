e66c98e763e9a114b2f52ec9b6a6e07be8016879
Fizzy <cameronca43@gmail.com>
2025-04-09 16:30:45 -0400

    Added hologram animation for swapping snapshots

--- Group 2 Project 1 - GAME 3400/Assets/Scripts/Final/Timeline.cs
+++ Group 2 Project 1 - GAME 3400/Assets/Scripts/Final/Timeline.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Timeline : MonoBehaviour
@@ -6,6 +7,11 @@ public class Timeline : MonoBehaviour
 
     public GameObject[] snapshots;
 
+    public AnimationCurve hologramAppearAnimation;
+    public float animationLength;
+
+    public Material[] hologramMaterials;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,8 +19,39 @@ public class Timeline : MonoBehaviour
     }
 
     public void SwitchToSnapshot(int index) {
+        StopAllCoroutines();
+        StartCoroutine(SwitchSnaphot(index));
+    }
+
+    IEnumerator SwitchSnaphot(int index) {
+        yield return StartCoroutine(AnimateHologram(false));
+        
         for(int i = 0; i < snapshots.Length; i++) {
             snapshots[i].SetActive(i == index);
+        }
+
+        yield return StartCoroutine(AnimateHologram(true));
+    }
+
+    IEnumerator AnimateHologram(bool isAppearing) {
+        float startTime = Time.time;
+
+        float timeSinceStart = Time.time - startTime;
+
+        while(timeSinceStart <= animationLength) {
+            float curveValue = hologramAppearAnimation.Evaluate(timeSinceStart / animationLength) * 10 - 1;
+
+            if(!isAppearing) {
+                curveValue = 5 - curveValue;
+            }
+
+            foreach(Material mat in hologramMaterials) {
+                mat.SetFloat("_Height_Cutoff", curveValue);
+            }
+                
+            
+            yield return new WaitForEndOfFrame();
+            timeSinceStart = Time.time - startTime;
         }
     }
 }
