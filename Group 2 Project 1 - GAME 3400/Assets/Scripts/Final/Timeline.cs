ce8705089b1703f4edc4c119ccac4863d3545f80
Fizzy <cameronca43@gmail.com>
2025-04-17 06:00:58 -0400

    Added railing and clock

--- Group 2 Project 1 - GAME 3400/Assets/Scripts/Final/Timeline.cs
+++ Group 2 Project 1 - GAME 3400/Assets/Scripts/Final/Timeline.cs
@@ -1,21 +1,48 @@
+using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class Timeline : MonoBehaviour
 {
+    [Serializable]
+    public class Snapshot {
+        public GameObject snapshot;
+        public int hour;
+        public int minute;
+    }
+
     public int startingSnapshotIndex;
 
-    public GameObject[] snapshots;
+    public Snapshot[] snapshots;
 
     public AnimationCurve hologramAppearAnimation;
     public float animationLength;
 
     public Material[] hologramMaterials;
 
+    public TMP_Text clock;
+
+    int curTime;
+
+
+    CanvasGroup canvas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        canvas = GetComponentInParent<CanvasGroup>();
+        canvas.alpha = 0;
         SwitchToSnapshot(startingSnapshotIndex);
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.F)) {
+            canvas.alpha = Mathf.Lerp(canvas.alpha, 1, 6 * Time.deltaTime);
+        }
+        else  {
+            canvas.alpha = Mathf.Lerp(canvas.alpha, 0, 18 * Time.deltaTime);
+        }
     }
 
     public void SwitchToSnapshot(int index) {
@@ -24,10 +51,11 @@ public class Timeline : MonoBehaviour
     }
 
     IEnumerator SwitchSnaphot(int index) {
+        StartCoroutine(AnimateTime(snapshots[index]));
         yield return StartCoroutine(AnimateHologram(false));
         
         for(int i = 0; i < snapshots.Length; i++) {
-            snapshots[i].SetActive(i == index);
+            snapshots[i].snapshot.SetActive(i == index);
         }
 
         yield return StartCoroutine(AnimateHologram(true));
@@ -39,7 +67,7 @@ public class Timeline : MonoBehaviour
         float timeSinceStart = Time.time - startTime;
 
         while(timeSinceStart <= animationLength) {
-            float curveValue = hologramAppearAnimation.Evaluate(timeSinceStart / animationLength) * 10 - 1;
+            float curveValue = hologramAppearAnimation.Evaluate(timeSinceStart / animationLength) * 8 - 1;
 
             if(!isAppearing) {
                 curveValue = 5 - curveValue;
@@ -53,5 +81,30 @@ public class Timeline : MonoBehaviour
             yield return new WaitForEndOfFrame();
             timeSinceStart = Time.time - startTime;
         }
+    }
+
+    IEnumerator AnimateTime(Snapshot snapshot) {
+        float startTime = Time.time;
+
+        float timeSinceStart = Time.time - startTime;
+
+        int targetTime = snapshot.hour * 60 + snapshot.minute;
+
+        while(timeSinceStart <= animationLength) {
+            float curveValue = hologramAppearAnimation.Evaluate(timeSinceStart / (animationLength * 2));
+
+            
+
+            clock.text = TimeToString((int)Mathf.Lerp(curTime, targetTime, curveValue));
+            
+            
+            yield return new WaitForEndOfFrame();
+            timeSinceStart = Time.time - startTime;
+        }
+        curTime = targetTime;
+    }
+
+    public string TimeToString(int time) {
+        return string.Format("{0}:{1:00}", time / 60, time % 60);
     }
 }
